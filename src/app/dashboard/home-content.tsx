edb1011f46daa4c9351bc8763c015d33f73917ac
Niels <niels@lucen.tech>
2026-06-16 10:04:39 +0200

    Floating add button alleen op mobile

--- src/app/dashboard/home-content.tsx
+++ src/app/dashboard/home-content.tsx
@@ -161,7 +161,7 @@ function FloatingAddButton({ onSelect }: { onSelect: (type: string) => void }) {
         />
       )}
 
-      <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3">
+      <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3 lg:hidden">
         {/* Options */}
         {open && (
           <div className="flex flex-col items-end gap-2">
